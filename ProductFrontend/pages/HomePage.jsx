1cb913ca722600c7f13f2168d53f1bac032ea18a
BarkInDaPark <matte_skater@hotmail.com>
2025-04-20 18:14:49 +0200

    adding to homepage

--- ProductFrontend/pages/HomePage.jsx
+++ ProductFrontend/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import styles from './homepage.module.css';
+import easterBanner from '../assets/easterBanner.jpg';
+
 
 function Homepage(){
     return (
         <div className={styles.container}>
             <h1 className={styles.h}>Welcome to product town! the place who got everything!</h1>
+            <div>
+                <img className={styles.image} src={easterBanner}></img>
+                <h1>Easter is here and we got the things you need to get the perfect easter</h1>
+            </div>
         </div>
     )
 }
