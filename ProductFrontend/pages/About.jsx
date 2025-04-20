27740cf2cc9713c18cc215bd96fc5a2f0e97ed26
BarkInDaPark <matte_skater@hotmail.com>
2025-04-20 12:35:23 +0200

    added visuals

--- ProductFrontend/pages/About.jsx
+++ ProductFrontend/pages/About.jsx
@@ -2,7 +2,7 @@ import styles from './about.module.css';
 
 function About() {
     return (
-        <div>
+        <div className={styles.container}>
             <h1>About our story:</h1>
             <p className={styles.para}>Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos.
 
