069b35b9d0c1dc68618142ffc1e3e132c1e573a2
BarkInDaPark <matte_skater@hotmail.com>
2025-04-22 22:22:22 +0200

    fixed sidebar style

--- ProductFrontend/navbar/Sidebar.jsx
+++ ProductFrontend/navbar/Sidebar.jsx
@@ -2,7 +2,7 @@ import styles from './sidebar.module.css';
 function Sidebar(){
     return(
         <div className={styles.container}>
-            <h1 className={styles.h}>category's</h1>
+            <h1 className={styles.h}>Category's</h1>
             <ul className={styles.list}>
                 <li className={styles.listItems}>Electronic</li>
                 <li className={styles.listItems}>Fitness</li>
