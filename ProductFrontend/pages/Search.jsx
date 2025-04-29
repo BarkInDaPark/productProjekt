c47c4a1ef673cb694e7d070d93ff673883b74133
BarkInDaPark <matte_skater@hotmail.com>
2025-04-29 15:55:08 +0200

    fixing search itemes

--- ProductFrontend/pages/Search.jsx
+++ ProductFrontend/pages/Search.jsx
@@ -10,18 +10,14 @@ const [product, setProduct] = useState({});
         <div className={styles.outerContainer}>
             <FetchSearch setProduct={setProduct}/>
              {!product ? <h1>Loading...</h1> :
-                             <div className={styles.innerContainer}>
-                                 {console.log(product)}
-                                 <div className={styles.innerInnerContainer}>
-                                     <h1 className={styles.cardHOne}>{product.name}</h1>
-                                     <p className= {styles.paraTwo}>Art nr: {product._id}</p>
-                                     <p className={styles.para}>{product.description}</p>
-                                     <h1 className={styles.cardHTwo}>{product.price}$</h1>
-                                 </div>
-                                 <img className={styles.cardImg} src={product.imageUrl} />
-                                 
-                             </div>
-                         }
+                product.map((product) => (
+                    <div className={styles.cardContainer} onClick={() => handleClick(product)} key={product._id}>
+                        <img className={styles.cardImg} src={product.imageUrl} />
+                        <h1 className={styles.cardHOne}>{product.name}</h1>
+                        <p className={styles.para}>{product.description}</p>
+                        <h1 className={styles.cardHTwo}>{product.price}$</h1>
+                    </div>))}
+                         
         </div>
     )
 }
