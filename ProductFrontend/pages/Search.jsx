90a8f4145bfbe9004125b13780a4a3bd2fcc416f
BarkInDaPark <matte_skater@hotmail.com>
2025-05-16 17:21:29 +0200

    fixed search visuals

--- ProductFrontend/pages/Search.jsx
+++ ProductFrontend/pages/Search.jsx
@@ -1,11 +1,12 @@
 import FetchSearch from '../components/fetchSearch';
-import styles from '../components/productsRendering.module.css';
+import styles from './search.module.css';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate, useParams} from 'react-router-dom'; 
 
 function Search(){
 
     const navigate = useNavigate();
+    const noProduct = `No products found with this search: "${useParams().id}"`;
 
     const handleClick = (product) => {
         console.log("product clicked: ", product._id);
@@ -20,14 +21,14 @@ const [product, setProduct] = useState([]);
     return(
         <div className={styles.container}>
             <FetchSearch setProduct={setProduct}/>
-             {!product ? <h1>Loading...</h1> :
+             {product.length === 0 ? <h1>{noProduct}</h1> :
                 product.map((product) => (
                     <div className={styles.cardContainer} onClick={() => handleClick(product)} key={product._id}>
                         <img className={styles.cardImg} src={product.imageUrl} />
                         <h1 className={styles.cardHOne}>{product.name}</h1>
                         <p className={styles.para}>{product.description}</p>
                         <h1 className={styles.cardHTwo}>{product.price}$</h1>
-                    </div>))}2
+                    </div>))}
                          
         </div>
     )
