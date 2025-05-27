a8f83f7f8dda7f021a4bc38a0058cfa5826e0457
BarkInDaPark <matte_skater@hotmail.com>
2025-05-27 23:29:20 +0200

    adding checkout form

---
+++ ProductFrontend/pages/Checkout.jsx
@@ -0,0 +1,27 @@
+import styles from './checkout.module.css';
+function Checkout(){
+    return (
+        <div className={styles.checkoutContainer}>
+            <form className={styles.checkoutForm}>
+                <p className={styles.inputParagraph}>first name:</p>
+                <input name='firstName' type='text' placeholder='First Name' required  className={styles.input}/>
+                <p className={styles.inputParagraph}>last name:</p>
+                <input name='lastName' type='text' placeholder='Last Name' required className={styles.input}/>
+                <p className={styles.inputParagraph}>Email:</p>
+                <input name='email' type='email' placeholder='Email' required className={styles.input}/>
+                <p className={styles.inputParagraph}>address:</p>
+                <input name='address' type='text' placeholder='Address' required className={styles.input}/>
+                <p className={styles.inputParagraph}>city:</p>
+                <input name='city' type='text' placeholder='City' required className={styles.input}/>
+                <p className={styles.inputParagraph}>postal code:</p>
+                <input name='postalCode' type='text' placeholder='Postal Code' required className={styles.input}/>
+                <p className={styles.inputParagraph}>country:</p>
+                <input name='country' type='text' placeholder='Country' required className={styles.input}/>
+                <p className={styles.inputParagraph}>phone number:</p>
+                <input name='phone' type='tel' placeholder='Phone Number' required className={styles.input}/>
+                <button type='submit' className={styles.submitButton}>Checkout</button>
+            </form>
+        </div>
+    )
+}
+export default Checkout;
